f90642cae56b6ba4be883395fe0b97567d7f2a6d
Yu <yumao@cmu.edu>
2017-04-22 17:14:46 -0400

    :)

--- THOR/get_thor_binaries.sh
+++ THOR/get_thor_binaries.sh
@@ -1,8 +1,8 @@
 mkdir -p thor_binary
 cd thor_binary
-wget https://s3-us-west-2.amazonaws.com/ai2-vision-robosims/builds/thor-cmu-201703101558-Linux64.zip
-wget https://s3-us-west-2.amazonaws.com/ai2-vision-robosims/builds/thor-cmu-201703101557-OSXIntel64.zip
-mkdir thor-cmu-201703101558-Linux64
+wget --no-check-certificate https://s3-us-west-2.amazonaws.com/ai2-vision-robosims/builds/thor-cmu-201703101558-Linux64.zip
+wget --no-check-certificate https://s3-us-west-2.amazonaws.com/ai2-vision-robosims/builds/thor-cmu-201703101557-OSXIntel64.zip
+mkdir -p thor-cmu-201703101558-Linux64
 unzip thor-cmu-201703101558-Linux64.zip -d thor-cmu-201703101558-Linux64
 unzip thor-cmu-201703101557-OSXIntel64.zip
 rm thor-cmu-201703101558-Linux64.zip
