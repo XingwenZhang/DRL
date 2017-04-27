c547feb74220561e5f65c48494ccffbd434a16d0
ccyang1992 <ccyang1992@icloud.com>
2017-04-27 15:30:46 +0000

    fix typo

--- THOR/THORUtils.py
+++ THOR/THORUtils.py
@@ -5,7 +5,7 @@ import cPickle as pickle
 # fast serialization with marshal
 
 def dump(obj, fd):
-	pickle.dump(obj, fd, protocal = pickle.HIGHEST_PROTOCOL)
+	pickle.dump(obj, fd, protocol = pickle.HIGHEST_PROTOCOL)
 
 def load(fd):
 	return pickle.load(fd)
